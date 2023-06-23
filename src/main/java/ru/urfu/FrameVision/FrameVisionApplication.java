eee4a46555d8ebc8279bf1069c181e79663b175d
AlibabaRazboinik <korall.sofiya@mail.ru>
2023-06-24 02:20:05 +0500

    quickstart

---
+++ src/main/java/ru/urfu/FrameVision/FrameVisionApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.FrameVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/urfu/FrameVision/FrameVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.FrameVision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
